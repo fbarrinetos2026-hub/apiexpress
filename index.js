e68f49da8982ba469cc51369259ec632255fe53f
fbarrientos26 <fbarrientos26@gmail.com>
2026-06-26 10:30:39 +0200

    reparacion de ruta post

--- index.js
+++ index.js
@@ -7,23 +7,27 @@ const PORT = 3000
 app.use(cors())
 app.use(express.json())
 
-const productos = [
-  { id: 1, nombre: 'Laptop', precio: 1200 },
-  { id: 2, nombre: 'Mouse', precio: 25 },
-  { id: 3, nombre: 'Teclado', precio: 45 },
-  { id: 4, nombre: 'Monitor', precio: 300 },
+const productos = [ 
+  { id: 1, nombre: 'Lapto', precio: 1200 },
+   { id: 2, nombre: 'mouse', precio: 25 },
+   { id: 3, nombre: 'teclado', precio: 45 },
+  { id: 4, nombre: 'moonitor', precio: 300 },
 ]
+//ruta de la api
 app.get('/api/productos', (req, res) => {
   res.json(productos)
 })
-
+//nuevo prodcuto
 app.post('/api/productos', (req, res) => {
-  const { nombre, precio } = req.body
-  const nuevo = { id: productos.length + 1, nombre, precio }
+  const nombre = req.body.nombre
+  const precio = req.body.precio
+
+  const nuevo = {id: productos.length + 1, nombre,precio}
   productos.push(nuevo)
-  res.status(201).json(nuevo)
+  res.json(nuevo)
 })
 
+//lanzando el server
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`)
 })
